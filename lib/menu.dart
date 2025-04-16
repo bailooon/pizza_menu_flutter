4054c1f7f812a8a4fb665394874dd517a357fd9b
bailooon <matheus@bailon.com.br>
2025-04-15 21:05:34 -0300

    configurando mensagens

--- lib/menu.dart
+++ lib/menu.dart
@@ -3,19 +3,50 @@ import 'package:pizza_menu/menu_item.dart';
 import 'package:pizza_menu/pizza_data.dart';
 
 class Menu extends StatelessWidget {
+  const Menu({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("Pizza Menu"),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              MenuItem(pizza: pizzaData[0],),
-              MenuItem(pizza: pizzaData[1],),
-            ],
+        body: Column(
+          children: [Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: pizzaData.map((pizza) => MenuItem(pizza: pizza)).toList(),
+              ),
+            ),
           ),
-        ));
+          if(isOpen())
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: ElevatedButton(
+              onPressed: () {}, 
+              child: Text("Order now!"),
+             ),
+          ),
+          ],
+        ),
+      );
+  }
+
+  bool isOpen() {
+    var horaInicial = 18;
+    var horaFinal = 23;
+    var horaAtual = DateTime.now().hour;
+
+    return horaAtual >= horaInicial && horaAtual < horaFinal;
+    
+  }
+
+  List<MenuItem> _buildPizzas(){
+    List<MenuItem> pizzas = [];
+    for(var pizza in pizzaData){
+      pizzas.add(MenuItem(pizza: pizza));
+    }
+
+    return pizzas;
   }
 }
--- lib/menu_item.dart
+++ lib/menu_item.dart
@@ -11,44 +11,44 @@ class MenuItem extends StatelessWidget {
       body: Column(
         children: [
           Card(
-            elevation: 2,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Image.asset(
+            elevation: 0,
+            child: (Row(
+              children: [
+                ColorFiltered(
+                  colorFilter: pizza.soldOut ? 
+                  ColorFilter.mode(Colors.grey, BlendMode.saturation) :
+                  ColorFilter.mode(Colors.transparent, BlendMode.saturation),
+                  child: Image.asset(
                     "images/${pizza.photoName}",
-                    width: 100,
-                    height: 100,
-                  ),
-                  SizedBox(
-                    width: 16,
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Texto 1",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          "Texto 2",
-                          style: TextStyle(fontSize: 16, color: Colors.black87),
-                        ),
-                        Text(
-                          "Texto 3",
-                          style: TextStyle(fontSize: 16, color: Colors.black54),
-                        )
-                      ],
-                    ),
-                  )
-                ],
+                  width: 100,
+                  height: 100,
+                ),
               ),
-            ),
+                SizedBox(width: 16,),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        pizza.name,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        pizza.ingredients,
+                        style: TextStyle(fontSize: 16, color: Colors.black87),
+                      ),
+                      Text(
+                        pizza.soldOut ? "Sold Out" : "\$${pizza.price}",
+                        style: TextStyle(fontSize: 16, color: Colors.black54),
+                      )
+                    ],
+                  ),
+                )
+              ],
+             ) ),
           ),
         ],
       ),
